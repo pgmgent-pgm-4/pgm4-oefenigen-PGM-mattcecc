70e7f1986037148ccf4b9382703048f338c0a67e
pgm-mattcecc <mattcecc@student.arteveldehs.be>
2024-04-22 12:39:14 +0200

    todo app

---
+++ week_02/exercises/todo/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TodoApp from './components/todo';
+function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
